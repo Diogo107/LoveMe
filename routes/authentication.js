6d807280f1ec7c8d403a362c6ef15e5be94b7b00
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-02-25 15:34:22 +0000

    mvp complete. Now the Styling.

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const { Router } = require('express');
-
 const passport = require('passport');
-
+const Animal = require('./../models/animal');
 const routeGuard = require('./../middleware/route-guard');
-
 const router = new Router();
 
 router.get('/sign-up', (req, res, next) => {
@@ -25,7 +23,11 @@ router.get('/sign-in', (req, res, next) => {
 });
 
 router.get('/profile', (req, res, next) => {
-  res.render('authentication/profile');
+  console.log(req.user._id);
+  let userId = req.user._id;
+  Animal.find({ user: `${userId}` }).then(animal => {
+    res.render('authentication/profile', { animal });
+  });
 });
 
 router.post(
