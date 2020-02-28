b317b4697840bc21d31cb05dc55807746ff9f6ff
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-02-28 14:22:03 +0000

    All done to be presentated

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const { Router } = require('express');
 const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
 const User = require('./../models/user');
+const Animal = require('./../models/animal');
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'PICKME!' });
@@ -20,6 +21,16 @@ router.get('/confirm-email/:userId', (req, res, next) => {
 
   User.findByIdAndUpdate(userId, validation).then(() => {
     res.render('authentication/profile');
+  });
+});
+
+router.get('/single-animal/profile/:userId', (req, res, next) => {
+  let userId = req.params.userId;
+  console.log('...............................................');
+  Animal.find({ user: `${userId}` }).then(animal => {
+    console.log(animal);
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    res.render('profile-open', { animal });
   });
 });
 
